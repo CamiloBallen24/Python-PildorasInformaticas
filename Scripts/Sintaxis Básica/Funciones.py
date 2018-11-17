cfd929e5c0cc3f064ceb863642c6b26e97e014d8
CamiloBallen24 <30605302+CamiloBallen24@users.noreply.github.com>
2018-11-17 10:31:29 -0500

    Funciones

---
+++ Scripts/Sintaxis Básica/Funciones.py
@@ -0,0 +1,21 @@
+#TEMA:FUNCIONES
+
+#Sintaxis
+#def nombre_funion(Parametros):
+	#Instrucciones de la Funcion
+	#return ->Opcional
+
+#Existen funciones predifinidas -> Vienen con el Lenguaje
+
+############################################################
+#Declaracion de la funcion
+def mensaje():
+	print("Estamos aprendiendo Python")
+	print("Estamos aprendiendo Instrucciones Basicas")
+	print("Poco a poco iremos aprendiendo")
+
+############################################################
+#Llamada de la funcion
+mensaje()
+print()
+mensaje()
