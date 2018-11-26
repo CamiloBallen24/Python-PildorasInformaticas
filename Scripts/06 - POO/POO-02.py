0de748f04dc28d95850d6854015dc583943e6f96
CamiloBallen24 <30605302+CamiloBallen24@users.noreply.github.com>
2018-11-26 09:45:27 -0500

    Encapsulacion

--- Scripts/06 - POO/POO-02.py
+++ Scripts/06 - POO/POO-02.py
@@ -1,5 +1,5 @@
 #Tema: -Estado Inicial (Constructor)
-#      -Encasuplacion
+#      -Encasuplacion (Atributos y Metodos)
 
 
 ##########################################################
@@ -15,9 +15,16 @@ class Coche():
 
 	def arrancar(self, arrancamos): 
 		self.__enMarcha=arrancamos
-	
+
 		if (self.__enMarcha):
+			chequeo = self.__chequeoInterno()
+	
+		if (self.__enMarcha and chequeo):
 			return "El coche esta en marcha"
+
+		elif(self.__enMarcha and chequeo==False):
+			return "Algo ha ido mal en el chequeo, no podemos arrancar"
+
 		else:
 			return "El coche esta parado"
 	
@@ -26,26 +33,41 @@ class Coche():
 		print("Un ancho de", self.__anchoChasis)
 		print("Un largo de", self.__largoChasis)
 	
+
+	#Metodo Ecapsulado
+	#__nombreMetodo, solo se puede acceder dentro de la clase
+	def __chequeoInterno(self):
+		print("Realizando Chequeo Interno")
+
+		self.gasolina = "ok"
+		self.aceite= "ok"
+		self.puertas = "cerradas"
+
+		if(self.gasolina == "ok" and self.aceite == "ok" and self.puertas == "cerradas"):
+			return True
+		else:
+			return False
+
 ########################################################## 
 
 
 ########################################################## 
 print("Ejemplo #1")
-
 print()
+
 print("Coche 1")
 miCoche01 = Coche()
-print(miCoche01.arrancar(True))
 miCoche01.estado()
-
+print(miCoche01.arrancar(True))
 print()
+print()
+
+
 print("Coche 2")
 miCoche02 = Coche()
-print(miCoche02.arrancar(False))
 miCoche02.estado()
+print(miCoche02.arrancar(False))
 
-print()
-print()
 print()
 print()
 print()
