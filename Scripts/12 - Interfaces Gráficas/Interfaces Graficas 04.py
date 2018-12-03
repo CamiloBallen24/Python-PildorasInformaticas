9b14bd47e396e20d35ccac96f6329b8ea4cef5cf
CamiloBallen24 <andrescamilo468@gmail.com>
2018-12-03 11:25:53 -0500

    Calculadora 1

--- Scripts/12 - Interfaces Gráficas/Interfaces Graficas 04.py
+++ Scripts/12 - Interfaces Gráficas/Interfaces Graficas 04.py
@@ -1,14 +1,15 @@
-#TEMA:  Widget Grid
+#TEMA: Text y Button
 
-#Trabajando con grillas o tablas
+#Text: Introducir textos largo
+#Button: Botones toda la vida
+
 ####################################################################
 from tkinter import *
 ####################################################################
 
-
 ####################################################################
 def codigoBoton():
-	mi_nombre.set("Camilo")
+	print("Boton Oprimido")
 ####################################################################
 
 
@@ -17,49 +18,6 @@ raiz = Tk()
 
 miFrame=Frame(raiz, width=1200, height=600)
 miFrame.pack()
-
-#Variable que usaremos en el codigo :v
-mi_nombre= StringVar()
-
-
-#Sticky alinea algo basandose en el sistemas cardinal (N, S, W, E) y puntos intermiedios
-#Padx y Pady, me permite determinar una distancia entre un objeto y su entorno
-
-#Creamos algunos label y unos cuadros de textos
-nombre_label= Label(miFrame, text="Nombre:")
-nombre_label.grid(row=0, column=0, sticky="w", padx=10, pady=10)
-
-
-apellido_label= Label(miFrame, text="Apellido:")
-apellido_label.grid(row=1, column=0, sticky="w", padx=10, pady=10)
-
-direccion_label= Label(miFrame, text="Direccion:")
-direccion_label.grid(row=2, column=0, sticky="w", padx=10, pady=10)
-
-
-#Le asociamos al cuadro de texto la variable mi_nombre
-cuadro_nombre = Entry(miFrame, textvariable=mi_nombre)
-cuadro_nombre.grid(row=0, column=1, padx=10, pady=10)
-cuadro_nombre.config(fg="red", justify="center")
-
-cuadro_apellido = Entry(miFrame)
-cuadro_apellido.grid(row=1, column=1, padx=10, pady=10)
-cuadro_apellido.config(fg="red", justify="center")
-
-cuadro_direccion = Entry(miFrame)
-cuadro_direccion.grid(row=2, column=1, padx=10, pady=10)
-cuadro_direccion.config(fg="red", justify="center")
-
-
-
-#Hacemos un campo de contraseña
-pass_label = Label(miFrame, text="Password: ")
-pass_label.grid(row=3, column=0, padx=10, pady=10 )
-
-cuadro_pass = Entry(miFrame)
-cuadro_pass.grid(row=3, column=1, padx=10, pady=10 )
-cuadro_pass.config(show="*")
-
 
 
 #Texto largo o Caja de Texto
@@ -80,14 +38,11 @@ scroll_vertical.grid(row=4, column=2, padx=10, pady=10, sticky="nswe")
 texto_comentarios.config(yscrollcommand=scroll_vertical.set)
 
 
+
 #Boton
 #Cuando se oprima el boton se ejecutara la funcion codigo Boton
 boton_envio= Button(raiz, text="Enviar", command=codigoBoton)
 boton_envio.pack()
 
-
-
 raiz.mainloop()
 ####################################################################
-
-
--- Scripts/12 - Interfaces Gráficas/Interfaces Graficas 05.py
+++ Scripts/12 - Interfaces Gráficas/Interfaces Graficas 05.py
@@ -1,15 +1,14 @@
-#TEMA: Text y Button
+#TEMA:  Widget Grid
 
-#Text: Introducir textos largo
-#Button: Botones toda la vida
-
+#Trabajando con grillas o tablas
 ####################################################################
 from tkinter import *
 ####################################################################
 
+
 ####################################################################
 def codigoBoton():
-	print("Boton Oprimido")
+	mi_nombre.set("Camilo")
 ####################################################################
 
 
@@ -18,6 +17,49 @@ raiz = Tk()
 
 miFrame=Frame(raiz, width=1200, height=600)
 miFrame.pack()
+
+#Variable que usaremos en el codigo :v
+mi_nombre= StringVar()
+
+
+#Sticky alinea algo basandose en el sistemas cardinal (N, S, W, E) y puntos intermiedios
+#Padx y Pady, me permite determinar una distancia entre un objeto y su entorno
+
+#Creamos algunos label y unos cuadros de textos
+nombre_label= Label(miFrame, text="Nombre:")
+nombre_label.grid(row=0, column=0, sticky="w", padx=10, pady=10)
+
+
+apellido_label= Label(miFrame, text="Apellido:")
+apellido_label.grid(row=1, column=0, sticky="w", padx=10, pady=10)
+
+direccion_label= Label(miFrame, text="Direccion:")
+direccion_label.grid(row=2, column=0, sticky="w", padx=10, pady=10)
+
+
+#Le asociamos al cuadro de texto la variable mi_nombre
+cuadro_nombre = Entry(miFrame, textvariable=mi_nombre)
+cuadro_nombre.grid(row=0, column=1, padx=10, pady=10)
+cuadro_nombre.config(fg="red", justify="center")
+
+cuadro_apellido = Entry(miFrame)
+cuadro_apellido.grid(row=1, column=1, padx=10, pady=10)
+cuadro_apellido.config(fg="red", justify="center")
+
+cuadro_direccion = Entry(miFrame)
+cuadro_direccion.grid(row=2, column=1, padx=10, pady=10)
+cuadro_direccion.config(fg="red", justify="center")
+
+
+
+#Hacemos un campo de contraseña
+pass_label = Label(miFrame, text="Password: ")
+pass_label.grid(row=3, column=0, padx=10, pady=10 )
+
+cuadro_pass = Entry(miFrame)
+cuadro_pass.grid(row=3, column=1, padx=10, pady=10 )
+cuadro_pass.config(show="*")
+
 
 
 #Texto largo o Caja de Texto
@@ -43,5 +85,9 @@ texto_comentarios.config(yscrollcommand=scroll_vertical.set)
 boton_envio= Button(raiz, text="Enviar", command=codigoBoton)
 boton_envio.pack()
 
+
+
 raiz.mainloop()
 ####################################################################
+
+
