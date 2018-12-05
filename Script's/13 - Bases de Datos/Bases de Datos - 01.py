ddc1eaed2568afa552edea54a63291e95b7b79dc
CamiloBallen24 <andrescamilo468@gmail.com>
2018-12-05 16:40:14 -0500

    Sin mensaje

--- Script's/13 - Bases de Datos/Bases de Datos - 01.py
+++ Script's/13 - Bases de Datos/Bases de Datos - 01.py
@@ -17,12 +17,37 @@ mi_cursor = mi_conexion.cursor()
 #mi_cursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
 
 #Agregamos datos
-mi_cursor.execute("INSERT INTO PRODUCTOS VALUES('BALON', 15, 'DEPORTES')")
+#mi_cursor.execute("INSERT INTO PRODUCTOS VALUES('BALON', 15, 'DEPORTES')")
+
+#Agregamos varios Datos
+'''
+varios_productos = [ 
+	("Camiseta", 10, "Deportes"), 
+	("Jarrón", 90, "Ceramica"),
+	("Camion", 20, "Jugueteria"), 
+	("Carro", 15, "Jugueteria")
+]
+
+mi_cursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", varios_productos)
+'''
+
+#Recibiendo datos
+mi_cursor.execute("SELECT * FROM PRODUCTOS")
+
+
+varios_productos=mi_cursor.fetchall() # -> Me regresa una lista con los productos
+
+for producto in varios_productos:
+	print("Nombre: ", producto[0], " \nPrecio: ", producto[1], " \nSeccion: ", producto[2], "\n\n")
+
 
 #Confirmamos los cambios
 mi_conexion.commit()
 
+
+
 #Cerramos el cursor o puntero
+
 
 #Cerramos la conexion
 mi_conexion.close()
