df4a6f438177a7a71451b39d18fdd6141dd83148
CamiloBallen24 <andrescamilo468@gmail.com>
2018-12-05 17:42:56 -0500

    Sin Mensaje

--- Script's/13 - Bases de Datos/Bases de Datos - 07.py
+++ Script's/13 - Bases de Datos/Bases de Datos - 07.py
@@ -1,19 +1,19 @@
-#TEMA: Identificador (ID)
+#TEMA: UNIQUE
 
 ###############################################################
 import sqlite3
 ###############################################################
 
 ###############################################################
-mi_conexion = sqlite3.connect("Gestion_productos_01")
+mi_conexion = sqlite3.connect("Gestion_productos_03")
 
 mi_cursor=mi_conexion.cursor()
 
 #Creacion tabla
 mi_cursor.execute('''
 	CREATE TABLE PRODUCTOS(
-	CODIGO_ARTICULO VARCHAR(4) PRIMARY KEY,
-	NOMBRE_ARTICULO VARCHAR(50),
+	ID INTEGER PRIMARY KEY AUTOINCREMENT,
+	NOMBRE_ARTICULO VARCHAR(50) UNIQUE,
 	PRECIO INTEGER,
 	SECCION VARCHAR(20))
 ''')
@@ -21,15 +21,15 @@ mi_cursor.execute('''
 #Archivos a insertar
 productos = [
 	
-	("AR01", "Pelota", 20, "Jugueteria"),
-	("AR02", "Pantalon", 15, "Confección"),
-	("AR03", "Destornillador", 25, "Ferretería"),
-	("AR04", "Jarrón", 45, "Cerámica")
+	("Pelota", 20, "Jugueteria"),
+	("Pantalon", 15, "Confección"),
+	("Destornillador", 25, "Ferretería"),
+	("Jarrón", 45, "Cerámica")
 	
 ]
 
 #Insercion
-mi_cursor.executemany("INSERT INTO PRODUCTOS VALUES (?, ?, ?, ?)", productos)
+mi_cursor.executemany("INSERT INTO PRODUCTOS VALUES (NULL, ?, ?, ?)", productos)
 
 
 
